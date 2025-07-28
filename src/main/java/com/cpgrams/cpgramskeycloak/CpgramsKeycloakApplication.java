1aabea99fe1a1071d278f159d57187d7f8cc8bd0
Darshit-Goml <darshit.dhameliya@goml.io>
2025-07-28 12:45:36 +0530

    set up

---
+++ src/main/java/com/cpgrams/cpgramskeycloak/CpgramsKeycloakApplication.java
@@ -0,0 +1,13 @@
+package com.cpgrams.cpgramskeycloak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpgramsKeycloakApplication {
+
+    public static void main( String[] args) {
+        SpringApplication.run(CpgramsKeycloakApplication.class, args);
+    }
+
+}
